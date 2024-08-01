ab75b222ad41746617fc372ce15988293b27a1b8
Sandro Soria <sandro33810@gmail.com>
2024-08-01 13:39:47 +0200

    Patch de la pagination ainsi que stylisation du tableau

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,23 @@ import '../app/css/style.css';
 import type { AppProps } from 'next/app';
 import Header from "@/components/ui/header";
 import Footer from "@/components/ui/footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import {useEffect} from "react";
+
+
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+    useEffect(() => {
+        AOS.init({
+            once: true,
+            disable: "phone",
+            duration: 700,
+            easing: "ease-out-cubic",
+        });
+    });
+
     return (
         <><Header/><Component {...pageProps} /><Footer/></>
     );
--- pages/download/index.tsx
+++ pages/download/index.tsx
@@ -2,24 +2,16 @@
 'use client'
 
 import { useState } from 'react';
+import Link from "next/link";
+import PageIllustration from "@/components/page-illustration";
 
 const data = [
-    {
-        id: 773,
-        name: 'secureeducrypt-1.20.1-773-alpha.zip',
-        hash: '15359ed36b96912d18c83077cb1a64a7',
-        date: '2024-07-26T06:00:44Z',
-        version: 'v1.20.1',
-        tag: 'Alpha',
-        type: 'SecureEduCrypt',
-        downloadLink: 'https://example.com/secureeducrypt-1.20.1-773-alpha.zip',
-        githubLink: 'https://github.com/your-repo/commit/773'
-    },
+
     {
         id: 774,
         name: 'secureedurest-1.20.1-773-alpha.zip',
         hash: '15359ed36b96912d18c83077cb1a64a7',
-        date: '2024-07-26T06:00:44Z',
+        date: '2024-07-26',
         version: 'v1.20.3',
         tag: 'Stable',
         type: 'SecureEduRest',
@@ -27,16 +19,72 @@ const data = [
         githubLink: 'https://github.com/your-repo/commit/773'
     },
     {
-        id: 775,
-        name: 'secureedumail-1.20.1-773-alpha.zip',
+        id: 774,
+        name: 'secureedurest-1.20.1-773-alpha.zip',
         hash: '15359ed36b96912d18c83077cb1a64a7',
-        date: '2024-07-26T06:00:44Z',
-        version: 'v1.20.2',
-        tag: 'Patch',
-        type: 'SecureEduMail',
+        date: '2024-07-26',
+        version: 'v1.20.3',
+        tag: 'Stable',
+        type: 'SecureEduRest',
         downloadLink: 'https://example.com/secureeducrypt-1.20.1-773-alpha.zip',
         githubLink: 'https://github.com/your-repo/commit/773'
     },
+    {
+        id: 774,
+        name: 'secureedurest-1.20.1-773-alpha.zip',
+        hash: '15359ed36b96912d18c83077cb1a64a7',
+        date: '2024-07-26',
+        version: 'v1.20.3',
+        tag: 'Stable',
+        type: 'SecureEduRest',
+        downloadLink: 'https://example.com/secureeducrypt-1.20.1-773-alpha.zip',
+        githubLink: 'https://github.com/your-repo/commit/773'
+    },
+    {
+        id: 774,
+        name: 'secureedurest-1.20.1-773-alpha.zip',
+        hash: '15359ed36b96912d18c83077cb1a64a7',
+        date: '2024-07-26',
+        version: 'v1.20.3',
+        tag: 'Stable',
+        type: 'SecureEduRest',
+        downloadLink: 'https://example.com/secureeducrypt-1.20.1-773-alpha.zip',
+        githubLink: 'https://github.com/your-repo/commit/773'
+    },
+    {
+        id: 774,
+        name: 'secureedurest-1.20.1-773-alpha.zip',
+        hash: '15359ed36b96912d18c83077cb1a64a7',
+        date: '2024-07-26',
+        version: 'v1.20.3',
+        tag: 'Stable',
+        type: 'SecureEduRest',
+        downloadLink: 'https://example.com/secureeducrypt-1.20.1-773-alpha.zip',
+        githubLink: 'https://github.com/your-repo/commit/773'
+    },
+    {
+        id: 774,
+        name: 'secureedurest-1.20.1-773-alpha.zip',
+        hash: '15359ed36b96912d18c83077cb1a64a7',
+        date: '2024-07-26',
+        version: 'v1.20.3',
+        tag: 'Stable',
+        type: 'SecureEduRest',
+        downloadLink: 'https://example.com/secureeducrypt-1.20.1-773-alpha.zip',
+        githubLink: 'https://github.com/your-repo/commit/773'
+    },
+    {
+        id: 774,
+        name: 'secureedurest-1.20.1-773-alpha.zip',
+        hash: '15359ed36b96912d18c83077cb1a64a7',
+        date: '2024-07-26',
+        version: 'v1.20.3',
+        tag: 'Stable',
+        type: 'SecureEduRest',
+        downloadLink: 'https://example.com/secureeducrypt-1.20.1-773-alpha.zip',
+        githubLink: 'https://github.com/your-repo/commit/773'
+    },
+
     // Ajoutez d'autres données ici pour le test
     // Ajoutez d'autres éléments pour tester la pagination
 ];
@@ -48,7 +96,7 @@ const DownloadPage = () => {
     const [selectedTag, setSelectedTag] = useState('Tag');
     const [selectedPeriod, setSelectedPeriod] = useState('Toute période');
     const [currentPage, setCurrentPage] = useState(1);
-    const itemsPerPage = 15;
+    const itemsPerPage = 6;
 
     const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
         const searchTerm = event.target.value.toLowerCase();
@@ -120,19 +168,29 @@ const DownloadPage = () => {
 
     return (
         <div className="container mx-auto p-4">
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
 
-            <div className="text-center mb-4">
-                <h1 className="text-center font-semibold">SecureEduMail</h1>
-                <p>Téléchargez les dernières versions ci-dessous</p>
+            <div className="text-center mb-6">
+                <h1 className="mb-6 border-y text-5xl font-bold [border-image:linear-gradient(to_right,transparent,theme(colors.slate.300/.8),transparent)1] md:text-6xl"
+                    data-aos="zoom-y-out" data-aos-delay={150}>
+                    SecureEduMail <br className="max-lg:hidden"/>
+                    Une plateforme éducative sécurisée pour les étudiants.
+                </h1>
+                <div className="mx-auto max-w-3xl">
+                    <p className="mb-8 text-lg text-gray-700" data-aos="zoom-y-out" data-aos-delay={300}>
+                        Vous pouvez retrouver tout les téléchargements de chaque référenciel juste ici.
+                    </p>
+                </div>
             </div>
 
+
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                <div className="flex flex-col sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4">
+                <div
+                    className="flex flex-col sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4">
                     <div className="flex items-center space-x-4">
                         <select
                             onChange={handleRepoTypeChange}
@@ -170,8 +228,10 @@ const DownloadPage = () => {
                     <label htmlFor="table-search" className="sr-only">Search</label>
                     <div className="relative">
                         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                            <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m1.5-2.5a7.5 7.5 0 1 0-15 0 7.5 7.5 0 0 0 15 0Z"/>
+                            <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                      stroke-width="2" d="m19 19-4-4m1.5-2.5a7.5 7.5 0 1 0-15 0 7.5 7.5 0 0 0 15 0Z"/>
                             </svg>
                         </div>
                         <input
@@ -206,20 +266,47 @@ const DownloadPage = () => {
                         <td className="px-6 py-4"><span
                             className="bg-indigo-100 text-indigo-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-indigo-900 dark:text-indigo-300">#{build.id}</span>
                         </td>
-                        <td className="px-6 py-4">{build.name}</td>
-                        <td className="px-6 py-4">{build.hash}</td>
-                        <td className="px-6 py-4">{build.date}</td>
+                        <td className="px-6 py-4 whitespace-nowrap overflow-hidden text-ellipsis"><span
+                            className="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">{build.name}</span>
+                        </td>
+                        <td className="px-6 py-4"><span
+                            className="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">{build.hash}</span>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap overflow-hidden text-ellipsis"><span
+                            className="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">{build.date}</span>
+                        </td>
                         <td className="px-6 py-4"><span
                             className="bg-purple-100 text-purple-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-purple-900 dark:text-purple-300">{build.version}</span>
                         </td>
                         <td className="px-6 py-4"><span
                             className="bg-indigo-100 text-indigo-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-indigo-400 border border-indigo-400">{build.type}</span>
                         </td>
-                        <td className="px-6 py-4">{build.tag}</td>
+                        <td className="px-6 py-4"><span
+                            className="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">{build.tag}</span>
+                        </td>
                         <td className="px-6 py-4"><a href={build.downloadLink}
-                                                     className="text-blue-600 hover:underline">Télécharger</a></td>
-                        <td className="px-6 py-4"><a href={build.githubLink}
-                                                     className="text-blue-600 hover:underline">GitHub</a></td>
+                                                     className="text-blue-600 hover:underline">
+                            <button type="button"
+                                    className="py-2 px-6 me-1 mb-1 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Télécharger
+                            </button>
+                        </a></td>
+                        <td className="px-6 py-8 flex justify-center items-center">
+                            <a href={build.githubLink} className="text-blue-600 hover:underline px-2"
+                               aria-label="GitHub">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 16 16"
+                                    width="16"
+                                    height="16"
+                                    fill="currentColor"
+                                    className="text-gray-600 hover:text-gray-800"
+                                >
+                                    <path
+                                        d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.01.08-2.1 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.03 2.2-.82 2.2-.82.44 1.09.16 1.9.08 2.1.51.56.82 1.28.82 2.15 0 3.07-1.87 3.75-3.64 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"/>
+                                </svg>
+                            </a>
+                        </td>
+
                     </tr>
 
                 ))}
@@ -245,7 +332,7 @@ const DownloadPage = () => {
                             Previous
                         </button>
                     </li>
-                    {Array.from({ length: totalPages }, (_, index) => (
+                    {Array.from({length: totalPages}, (_, index) => (
                         <li key={index}>
                             <button
                                 onClick={() => handlePageChange(index + 1)}
