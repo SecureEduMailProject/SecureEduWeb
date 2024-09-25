14b44cea0367e0f5c91b5f300c27f32b2ae2ebb1
Sandro Soria <sandro33810@gmail.com>
2024-09-25 14:16:18 +0200

    Update text EN -> FR

--- pages/download/index.tsx
+++ pages/download/index.tsx
@@ -268,9 +268,9 @@ const DownloadPage = () => {
                  aria-label="Table navigation">
                 <span
                     className="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">
-                    Showing <span
-                    className="font-semibold text-gray-900 dark:text-white">{(currentPage - 1) * itemsPerPage + 1}</span> to <span
-                    className="font-semibold text-gray-900 dark:text-white">{Math.min(currentPage * itemsPerPage, filteredBuilds.length)}</span> of <span
+                    Montre <span
+                    className="font-semibold text-gray-900 dark:text-white">{(currentPage - 1) * itemsPerPage + 1}</span> vers <span
+                    className="font-semibold text-gray-900 dark:text-white">{Math.min(currentPage * itemsPerPage, filteredBuilds.length)}</span> de <span
                     className="font-semibold text-gray-900 dark:text-white">{filteredBuilds.length}</span>
                 </span>
                 <ul className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
@@ -280,7 +280,7 @@ const DownloadPage = () => {
                             disabled={currentPage === 1}
                             className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                         >
-                            Previous
+                            Précédent
                         </button>
                     </li>
                     {Array.from({length: totalPages}, (_, index) => (
@@ -299,7 +299,7 @@ const DownloadPage = () => {
                             disabled={currentPage === totalPages}
                             className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                         >
-                            Next
+                            Suivant
                         </button>
                     </li>
                 </ul>
